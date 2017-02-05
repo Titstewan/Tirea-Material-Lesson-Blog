37c7859422fd10bcec4cdce5ee1c9e3e89be60b0
Tìtstewan <titstewan@learnnavi.org>
2017-02-05 17:16:56 +0100

    Initial release

---
+++ generate.php
@@ -0,0 +1,348 @@
+<?php
+// Some php functions for generating the site
+if (!defined('TLB'))
+	die('No direct access...');
+
+// So...
+function name_gen()
+{
+echo '
+<div>
+<div class="titlename">Random Na&apos;vi Name: </div><br>
+	<br>
+<h4>';
+$a = $_REQUEST['a']; $b = $_REQUEST['b']; $c = $_REQUEST['c'];
+$hrh = $_REQUEST['hrh'];
+
+if (($a > 4 || $b > 4 || $c > 4) && !isset($hrh))
+	echo 'HRH te HRH HRH\'itan';
+else {
+	if (isset($hrh) && $hrh != 1)
+		echo 'HRH te HRH HRH\'ite';
+	else
+	{
+	function getInitial() {
+				$type;
+				$result;
+
+				if (rand(0,100) <= 70){
+					$type = 'single';
+				}else {
+					$type = 'cluster';
+				}
+
+				//single letter initial
+				if ($type == 'single'){
+					$rn = rand(0, 100);
+					if ($rn <= 4){
+						$result = 'px';
+					}else if ($rn <= 8){
+						$result = 'tx';
+					}else if ($rn <= 12){
+						$result = 'kx';
+					}else if ($rn <= 17){
+						$result = 'p';
+					}else if ($rn <= (22)){
+						$result = 't';
+					}else if ($rn <= (27)){
+						$result = 'k';
+					}else if ($rn <= (32)){
+						$result = 'ts';
+					}else if ($rn <= (37)){
+						$result = 'f';
+					}else if ($rn <= (42)){
+						$result = 's';
+					}else if ($rn <= (47)){
+						$result = 'h';
+					}else if ($rn <= (52)){
+						$result = 'v';
+					}else if ($rn <= (57)){
+						$result = 'z';
+					}else if ($rn <= (62)){
+						$result = 'm';
+					}else if ($rn <= (67)){
+						$result = 'n';
+					}else if ($rn <= (72)){
+						$result = 'ng';
+					}else if ($rn <= (77)){
+						$result = 'r';
+					}else if ($rn <= (82)){
+						$result = 'l';
+					}else if ($rn <= (87)){
+						$result = 'w';
+					}else if ($rn <= (92)){
+						$result = 'n';
+					}else {
+						$result = '\'';
+					}
+				}else {
+					$ro = rand(1, 3);
+					//start with f
+					if ($ro == 1){
+						$rp = rand(1, 100);
+						if ($rp <= 5){
+							$result = 'fpx';
+						}else if ($rp <= 11){
+							$result = 'fkx';
+						}else if ($rp <= 16){
+							$result = 'ftx';
+						}else if ($rp <= 25){
+							$result = 'ft';
+						}else if ($rp <= 33){
+							$result = 'fp';
+						}else if ($rp <= 42){
+							$result = 'fk';
+						}else if ($rp <= 50){
+							$result = 'fm';
+						}else if ($rp <= 57){
+							$result = 'fn';
+						}else if ($rp <= 63){
+							$result = 'fng';
+						}else if ($rp <= 70){
+							$result = 'fr';
+						}else if ($rp <= 78){
+							$result = 'fl';
+						}else if ($rp <= 86){
+							$result = 'fw';
+						}else if ($rp <= 94){
+							$result = 'fy';
+						}else {
+							$result = 'fr';
+						}
+					}else if ($ro == 2){ //start with s
+						$rp = rand(1, 100);
+						if ($rp <= 5){
+							$result = 'spx';
+						}else if ($rp <= 11){
+							$result = 'skx';
+						}else if ($rp <= 16){
+							$result = 'stx';
+						}else if ($rp <= 25){
+							$result = 'st';
+						}else if ($rp <= 33){
+							$result = 'sp';
+						}else if ($rp <= 42){
+							$result = 'sk';
+						}else if ($rp <= 50){
+							$result = 'sm';
+						}else if ($rp <= 57){
+							$result = 'sn';
+						}else if ($rp <= 63){
+							$result = 'sng';
+						}else if ($rp <= 70){
+							$result = 'sr';
+						}else if ($rp <= 78){
+							$result = 'sl';
+						}else if ($rp <= 86){
+							$result = 'sw';
+						}else if ($rp <= 94){
+							$result = 'sy';
+						}else {
+							$result = 'sr';
+						}
+					}
+					elseif ($ro == 3)
+					{ //start with ts
+						$rp = rand(1, 100);
+						if ($rp <= 5){
+							$result = 'tspx';
+						}else if ($rp <= 11){
+							$result = 'tskx';
+						}else if ($rp <= 16){
+							$result = 'tstx';
+						}else if ($rp <= 25){
+							$result = 'tst';
+						}else if ($rp <= 33){
+							$result = 'tsp';
+						}else if ($rp <= 42){
+							$result = 'tsk';
+						}else if ($rp <= 50){
+							$result = 'tsm';
+						}else if ($rp <= 57){
+							$result = 'tsn';
+						}else if ($rp <= 63){
+							$result = 'tsng';
+						}else if ($rp <= 70){
+							$result = 'tsr';
+						}else if ($rp <= 78){
+							$result = 'tsl';
+						}else if ($rp <= 86){
+							$result = 'tsw';
+						}else if ($rp <= 94){
+							$result = 'tsy';
+						}else {$result = 'tsr';}
+					}
+				}
+				return $result;
+			}
+
+	function getNucleus() {
+
+				$isDiphthong;
+				$result;
+
+				if (rand(0,100) > 20){
+					$isDiphthong='kehe';
+				}else {
+					$isDiphthong='srane';
+				}
+
+				if ($isDiphthong == 'srane'){ //diphthong
+					$rx = rand(0, 100);
+					if ($rx <= 25){
+						$result = 'aw';
+					}else if ($rx <= 50){
+						$result = 'ay';
+					}else if ($rx <= 75){
+						$result = 'ey';
+					}else if ($rx <= 100){
+						$result = 'ew';
+					}
+				}else {
+					$ry = rand(1, 100);
+					if ($ry <= 25){
+						$result = 'a';
+					}else if ($ry <= 40){
+						$result = 'e';
+					}else if ($ry <= 55){
+						$result = 'o';
+					}else if ($ry <= 70){
+						$result = 'u';
+					}else if ($ry <= 80){
+						$result = 'ì';
+					}else if ($ry <= 85){
+						$result = 'ä';
+					}else {$result = 'a';}
+				}
+				return $result;
+			}
+
+	function getCoda () {
+
+				$result;
+				$rz = rand(0, 320);
+
+				if ($rz <= 4){
+					$result = 'px';
+				}else if ($rz <= 8){
+					$result = 'tx';
+				}else if ($rz <= 12){
+					$result = 'kx';
+				}else if ($rz <= 20){
+					$result = 'p';
+				}else if ($rz <= 28){
+					$result = 't';
+				}else if ($rz <= 44){
+					$result = 'k';
+				}else if ($rz <= 49){
+					$result = 'k';
+				}else if ($rz <= 58){
+					$result = 'm';
+				}else if ($rz <= 70){
+					$result = 'n';
+				}else if ($rz <= 76){
+					$result = 'ng';
+				}else if ($rz <= 80){
+					$result = 'r';
+				}else if ($rz <= 85){
+					$result = 'l';
+				}else{
+					$result='';
+				}
+				return $result;
+			}
+
+			$k;
+			while ($k <= $_REQUEST['k'] -5){
+				$i=0;
+				echo ucfirst(getInitial().getNucleus());
+				while ($i <= $a - 2){
+					echo getInitial().getNucleus();
+					$i++;
+				}
+				echo getCoda();$i=0;
+				echo ' te ';
+				echo ucfirst(getInitial().getNucleus());
+				while ($i <= $b - 2){
+					echo getInitial().getNucleus();
+					$i++;
+				}
+				echo getCoda();
+				$i=0;
+				echo ' ';
+				echo ucfirst(getInitial().getNucleus());
+				while ($i <= $c - 2){
+					echo getInitial().getNucleus();
+					$i++;
+				}
+				echo getCoda();$i=0;
+				echo '\'it';
+				if (rand(0,1)==0){
+					echo 'an';
+				}else{
+					echo 'e';
+				}
+				$k++;
+				//echo '<br/>';
+			}
+		}
+}
+echo '
+</h4>';
+if (($a > 4 || $b > 4 || $c > 4) && (!isset($hrh) || $hrh != 1) ) {
+	echo '<span class="cyan-text darken-3">Nice try. ;D</span>';
+}
+echo '
+<br />
+<hr>
+
+<style>select{display:block;}</style>
+<div class="row">
+  <form class="col s12" name="sform">
+		<div class="row">
+			<div class="input-field col s12 l4">
+			  <select name="a">
+			    <option value="" disabled selected>First Name # of Syllables</option>
+			    <option value="1">1</option>
+			    <option value="2">2</option>
+			    <option value="3">3</option>
+			    <option value="4">4</option>
+			  </select>
+			</div>
+		</div>
+		<div class="row">
+			<div class="input-field col s12 l4">
+			  <select name="b">
+			    <option value="" disabled selected>Family Name # of Syllables</option>
+			    <option value="1">1</option>
+			    <option value="2">2</option>
+			    <option value="3">3</option>
+			    <option value="4">4</option>
+			  </select>
+			</div>
+		</div>
+		<div class="row">
+			<div class="input-field col s12 l4">
+			  <select name="c">
+			    <option value="" disabled selected>Parent&apos;s Name # of Syllables</option>
+			    <option value="1">1</option>
+			    <option value="2">2</option>
+			    <option value="3">3</option>
+			    <option value="4">4</option>
+			  </select>
+			</div>
+		</div>
+		<button class="btn waves-effect waves-light amber black-text" type="submit">Submit
+    	<i class="material-icons right">send</i>
+		</button>
+	</form>
+</div>
+<div style="margin-top: 18px; text-align: center; border-top: 1px solid #eeeeee; padding-top: 5px;">
+	<a href="http://forum.learnnavi.org/index.php?msg=566249.0">
+		Web-based Na\'vi Name Generator!
+	</a> by Uniltìrantokx te Skxawng
+</div>
+</div>';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+// For some reasons, we will have to set up a memory limit to keep the server happy
+@ini_set('memory_limit', '8M');
+
+// This is kind of important.
+define('TLB', 1);
+
+$sourcedir = dirname(__FILE__);
+
+// Emit some headers for some modicum of protection against nasties.
+if (!headers_sent())
+{
+	// Future versions will make some of this configurable. This is primarily a 'safe' configuration for most cases for now.
+	header('X-Frame-Options: SAMEORIGIN');
+	header('X-XSS-Protection: 1');
+	header('X-Content-Type-Options: nosniff');
+}
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+// Let's require the source file!
+require_once($sourcedir . '/source.php');
+
+// Call the main functions, woo!
+// The <html> start tag and the buttors for Na'vigation
+html_header();
+
+// The Homepage
+// What function shall we execute? (done like this for memory's sake.)
+call_user_func(hp_main());
+
+// The main controlling function.
+function hp_main()
+{
+	global $sourcedir;
+
+	// Here's the $_REQUEST['page'] array - $_REQUEST['page'] => array($file, $function).
+	$pageArray = array(
+		'sounds' => array('source.php', 'abc_sound'),
+		'generator' => array('generate.php', 'name_gen'),
+		'links' => array('source.php', 'aysaheylu'),
+		'downloads' => array('source.php', 'navi_download'),
+		'lessons' => array('source.php', 'navi_lesson'),
+	);
+
+	// Get the function and file to include - if it's not there, do the index.
+	if (!isset($_REQUEST['page']) || !isset($pageArray[$_REQUEST['page']]))
+	{
+		// Fall through to the index then...
+		require_once($sourcedir . '/index.php');
+		return 'home';
+	}
+
+	// Otherwise, it was set - so let's go to that page.
+	require_once($sourcedir . '/' . $pageArray[$_REQUEST['page']][0]);
+	return $pageArray[$_REQUEST['page']][1];
+}
+
+// HTML end </html> plus the disclaimer
+html_bottom();
+
+?>
