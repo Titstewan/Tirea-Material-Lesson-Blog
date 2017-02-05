eeb2e4724d71d2a728fef36c59fbb33c28e6fdf1
Tìtstewan <titstewan@learnnavi.org>
2017-02-05 17:22:28 +0100

    Initial release

---
+++ download/index.php
@@ -0,0 +1,170 @@
+﻿<?php include "../res/themetop.php" ?>
+<div class="titlename">Downloads</div>
+<!-- <h4>Lessons as PDF</h4>
+<div class="row">
+  <div class="col s12 m3">
+    <a href="/material/download/1.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        1. Order, -l &amp; -t
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/2.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        2. Past and Future
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/3.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        3. Have, Plurals, Changes
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/4.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        4. Describing Stuff
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/5.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        5. Questions
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/6.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        6. This &amp; That +misc
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/7.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        7. -ru, -yä, and si
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/8.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        8. &lt;iv&gt;
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/9.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        9. -ri
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/10.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        10. No and Not
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/11.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        11. Pum
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/12.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        12. -ing in Na&#39;vi
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/13.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        13. F-words
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/14.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        14. Colours
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/15.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        15. Numbers
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/16.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        16. &lt;eyk&gt;
+        </span>
+      </div>
+    </a>
+  </div>
+  <div class="col s12 m3">
+    <a href="/material/download/17.pdf">
+      <div class="card-panel cyan darken-2">
+        <span class="white-text">
+        17. Self
+        </span>
+      </div>
+    </a>
+  </div>
+</div> -->
+<ul class="collection with-header">
+  <li class="collection-header"><h4>Other Things Hosted On This Site</h4></li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/101-Handout-4-WA-2012.pdf">Na'vi 101 Handout [PDF]</a><br>From AvatarMeet 2012 Seattle, WA</li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/102-Handout-4-DC-2013.pdf">Na'vi 102 Handout [PDF]</a><br>From AvatarMeet 2013 Washington, DC</li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/103-Handout-4-LA-2014.pdf">Na'vi 103 Handout [PDF]</a><br>From AvatarMeet 2014 Los Angeles, CA</li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/hkb-kxwerty-mod.apk">Hacker&#39;s Keyboard Mod [APK Android App]</a><br>KXWERTY keyboard layout for Android</li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/ts3_Client_LN_Sound_Pack-0.2.zip">TS3 Voice Pack [ZIP]</a><br>Na&#39;vi Language support for TeamSpeak 3</li>
+  <li class="collection-item"><a class="collection-link" href="/material/download/navkb6.zip">The KXWERTY Keyboard [ZIP]</a><br>KXWERTY keyboard layout for Windows / Mac</li>
+  <li class="collection-item"><a class="collection-link" href="/material/generate.php">Na&#39;vi Name Generator</a><br>Generates valid names</li>
+  <!-- <li class="collection-item"><a class="collection-link" href="/material/img">images folder</a><br>Bunch of old memes and comics and whatnot, some Na&#39;vi-related</li> -->
+</ul>
+<?php include "../res/themebottom.php" ?>
