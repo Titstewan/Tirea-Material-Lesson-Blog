0d90d8537d9be1be8b39cdc2fb72fd8a31e32e62
Tirea Aean <tirea@learnnavi.org>
2017-03-12 22:31:53 -0500

    clean up lessons index even more

--- source.php
+++ source.php
@@ -653,20 +653,14 @@ function navi_lesson($lnum)
 		<div class="titlename">Na\'vi Language Lessons</div>
 		<br><br>
 
-		<h1>Conversational Lessons</h1>
-
 		<ul class="collection with-header">
-			<li class="collection-header"><h4>Beginners\' Basics</h4></li>
+			<li class="collection-header"><h4>Conversational Lessons: Beginners\' Basics</h4></li>
 			<li class="collection-item"><a class="collection-link" href="#">Greetings &amp; Introductions</a></li>
 			<li class="collection-item"><a class="collection-link" href="#">What are you doing?</a></li>
 		</ul>
 
-		<h1>Simplified Grammar Lessons</h1>
-
-		<p>While I\'m working on new content and the back end of this website, here are the classic lessons. Tried and true over the years, and now back by popular demand.</p>
-
 		<ul class="collection with-header">
-			<li class="collection-header"><h4>Intro to Na\'vi Grammar</h4></li>
+			<li class="collection-header"><h4>Simplified Grammar Lessons: Intro to Na\'vi Grammar</h4></li>
 			<li class="collection-item"><a class="collection-link" href="#">Word Order &amp; Simple Sentences</a></li>
 		</ul>
 		';
