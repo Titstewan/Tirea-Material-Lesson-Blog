50625f60cc32ab63e23a834bb2c380b3e78ec028
Tirea Aean <tirea@learnnavi.org>
2017-03-12 22:27:03 -0500

    clean up lessons index

--- source.php
+++ source.php
@@ -653,40 +653,21 @@ function navi_lesson($lnum)
 		<div class="titlename">Na\'vi Language Lessons</div>
 		<br><br>
 
-		<!-- <h1>Conversational Lessons</h1>
+		<h1>Conversational Lessons</h1>
 
 		<ul class="collection with-header">
 			<li class="collection-header"><h4>Beginners\' Basics</h4></li>
-			<li class="collection-item"><a class="collection-link" href="01.php">Greetings &amp; Introductions</a></li>
-			<li class="collection-item"><a class="collection-link" href="02.php">What are you doing?</a></li>
-		</ul> -->
+			<li class="collection-item"><a class="collection-link" href="#">Greetings &amp; Introductions</a></li>
+			<li class="collection-item"><a class="collection-link" href="#">What are you doing?</a></li>
+		</ul>
 
 		<h1>Simplified Grammar Lessons</h1>
 
 		<p>While I\'m working on new content and the back end of this website, here are the classic lessons. Tried and true over the years, and now back by popular demand.</p>
 
 		<ul class="collection with-header">
-			<!-- <li class="collection-header"><h4>Intro to Na\'vi Grammar</h4></li> -->
-			<!-- <li class="collection-item"><a class="collection-link" href="01g.php">Word Order &amp; Simple Sentences</a></li> -->
-			<!-- old lessons -->
-			<li class="collection-header"><h4>Classic Tirea Na\'vi Lessons</h4></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/01c.php">01: Order, -l &amp; -t</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/02c.php">02: Past and Future</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/03c.php">03: Have, Plurals, Changes</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/04c.php">04: Describing Stuff</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/05c.php">05: Questions</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/06c.php">06: This &amp; That +misc</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/07c.php">07: -ru, -yä, and si</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/08c.php">08: &lt;iv&gt;</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/09c.php">09: -ri</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/10c.php">10: No and Not</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/11c.php">11: Pum</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/12c.php">12: -ing in Na&#39;vi</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/13c.php">13: F-words</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/14c.php">14: Colours</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/15c.php">15: Numbers</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/16c.php">16: &lt;eyk&gt;</a></li>
-			<li class="collection-item"><a class="collection-link" href="lessons/17c.php">17: Self</a></li>
+			<li class="collection-header"><h4>Intro to Na\'vi Grammar</h4></li>
+			<li class="collection-item"><a class="collection-link" href="#">Word Order &amp; Simple Sentences</a></li>
 		</ul>
 		';
 	}
