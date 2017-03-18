e1bf1ff879f9442ccdef300f4ae8dcd93bb8ff00
Tìtstewan <titstewan@learnnavi.org>
2017-03-18 19:45:37 +0100

    Added an https redirect function :D fix typo

--- index.php
+++ index.php
@@ -46,7 +46,6 @@ if (!headers_sent())
 if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off')
 {
 	$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	header('HTTP/1.1 301 Moved Permanently');
 	header('Location: ' . $redirect);
 	exit();
 }
