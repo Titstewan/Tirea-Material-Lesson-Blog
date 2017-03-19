7aee73a9e57ec66b6e908f6c936397544f2b4da1
Tìtstewan <titstewan@learnnavi.org>
2017-03-19 01:36:34 +0100

    The german text strings!

---
+++ language/german.php
@@ -0,0 +1,74 @@
+<?php
+/*----------------------------
+This is a new custom home page for Tirea Tean's Lesson Blog.
+
+The main Text Strings for translation.
+
+Author: Tìtstewan
+titstewan-learnnavi.org
+Co-Author: Tirea Aean
+tirea-learnnavi.org
+----------------------------*/
+// $txt[''] = '';
+// general stuff
+$txt['no_audio'] = 'Dein Browser unterstützt das Audioelement nicht.';
+$txt['tlb_exp'] = 'EXPERIMENTELLE SEITE IM AUFBAU! :D';
+$txt['foot_disc'] = 'Webseiten Administrator/Designer: <a href="http://forum.learnnavi.org/profile/?u=1975">Tirea Aean</a>,
+	<span title="PHP: Hypertext Preprocessor">Software Entwicklung</span>:
+	<a href="http://forum.learnnavi.org/profile/?u=10322">Tìtstewan</a> und 
+	<a href="http://forum.learnnavi.org/profile/?u=1975">Tirea Aean</a>
+    | Diese Seite wurde von den Mitgliedern des <a href="http://learnnavi.org/">LearnNa\'vi.org</a> Forums erstellt. <br />
+    Diese Seite ist nicht zugehöring zur offiziellen Avatar Webseite, James Cameron, Lightstorm Entertainment oder Twentieth Century Fox Film Corporation.
+	Alle Schutzmarken und Servicemarken sind Eigentum ihrer jeweiligen Besitzer.<br />
+    <b>\'Ivong Na\'vi!</b>';
+// Tuto: h_ = home, m_ = menu, s_ = sound page, g_ = the name generator, l_ = links, d_ = downloads, n_ = Na'vi lessons
+// menu
+$txt['m_home'] = 'Home';
+$txt['m_sounds'] = 'Klänge';
+$txt['m_lessons'] = 'Unterricht';
+$txt['m_links'] = 'Links';
+$txt['m_downloads'] = 'Downloads';
+$txt['m_rss'] = 'RSS Feed';
+// home
+$txt['h_welcome'] = 'Wilkommen';
+$txt['h_title'] = 'Einfacher Grammatik Unterricht';
+$txt['h_welcome_txt'] = 'Wilkommen bei Tirea Na\'vi! Diese Seite gibt es, um die Sprache der Na\'vi einfach und verständlich für jederman zu lehren.
+                  Für alle nicht-Linguisten Avatar Fans da draußen, Ich hoffe, dass ich helfen kann bei eurem Weg der nächste Sprecher dieser erstaunlichen Sprache zu sein.
+                  -- Tirea Aean';
+$txt['h_get_st'] = 'Erste Schritte';
+// sounds
+$txt['s_sound'] = 'Na&#39;vi Klänge';
+$txt['s_intro'] = '<span class="navi">Kaltxì</span> ma frapo!
+	Diese Seite beinhaltet Buchstaben, dazugehörigen International Phonetics Alphabet (IPA) Zeichen und Audioaufnahmen der Sprache der Na\'vi.
+	Nun, um deine Aussprache zu trainieren, klicke auf das Abspielen-Button und höre. Versuche, mit zu machen.';
+$txt['s_vowels'] = 'Vokale';
+$txt['s_pseudo'] = 'Pseudovokale';
+$txt['s_conson'] = 'Konsonanten';
+$txt['s_diphto'] = 'Diphthonge';
+// links
+$txt['l_links'] = 'Links';
+$txt['l_cool'] = 'Dinge, die ich cool finde';
+$txt['l_blogs'] = 'Na\'vi Blogs, die ich lese';
+$txt['l_navi_gen'] = 'Na\'vi Namen Generator';
+$txt['l_valid_gen'] = 'Generiert valide Namen';
+// downloads
+$txt['d_downl'] = 'Downloads';
+$txt['d_thing'] = 'Andere Dinge, zur Verfügung gestellt auf dieser Seite';
+$txt['d_hrhgif'] = 'Bunch of old memes and comics and whatnot, some Na&#39;vi-related';
+$txt['d_images'] = 'Bilderordner';
+// lessons
+$txt['n_lesson'] = 'Na\'vi Sprachunterricht';
+$txt['n_basic'] = 'Konversationsunterricht: Anfänger Grundlagen';
+$txt['n_kaltxi'] = 'Grüße &amp; Vorstellung';
+$txt['n_intro'] = 'Vereinfachter Grammatik-Unterricht: Einführung der Na\'vi Grammatik';
+// name generator
+$txt['g_n_try'] = 'Netter Versuch. ;D';
+$txt['g_first_name'] = 'Vorname # der Silben';
+$txt['g_family_name'] = 'Familienname # der Silben';
+$txt['g_parent_name'] = 'Elternname # der Silben';
+$txt['g_number_of_name'] = 'Anzahl der zu generierende Namen';
+$txt['g_credits'] = '<a href="http://forum.learnnavi.org/index.php?msg=566249">
+      Web-basierender Na\'vi Namen Generator!
+    </a> von Uniltìrantokx te Skxawng';
+$txt['g_generate'] = 'Generiere!';
+?>
