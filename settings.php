9b95ad1670b26492e66f0c4a9f4a442914d2ba48
Tìtstewan <titstewan@learnnavi.org>
2017-03-19 04:53:28 +0100

    We need this because of reasons...

---
+++ settings.php
@@ -0,0 +1,26 @@
+<?php
+/*----------------------------
+This is a new custom home page for Tirea Tean's Lesson Blog.
+
+Control center
+
+Here are no functions, but a place for the definition of important variables.
+
+Author: Tìtstewan
+titstewan-learnnavi.org
+Co-Author: Tirea Aean
+tirea-learnnavi.org
+----------------------------*/
+if (!defined('TLB'))
+	die('No direct access...');
+
+// define the dir
+$sourcedir = dirname(__FILE__);
+
+// path to the lesson dir
+$lessondir = $sourcedir . '/lessons';
+
+// define the weblink
+$weblink = '/material/index.php';
+
+?>
