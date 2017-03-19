4c7f35478c7197debdcb7bcf24b574633eb3d3a8
Tìtstewan <titstewan@learnnavi.org>
2017-03-19 04:37:50 +0100

    fixed something weird

--- lessons/index.php
+++ lessons/index.php
@@ -1,11 +1,24 @@
 <?php
 // This file is here solely to protect this directory.
+define('TLB', 1);
 // Look for index.php....
-if (file_exists(dirname(dirname(__FILE__)) . '/index.php'))
+if (file_exists(dirname(dirname(__FILE__)) . '/settings.php'))
 {
 	// Found it!
-	require(dirname(dirname(__FILE__)) . '/index.php');
-	header('Location: ' . $weblink);
+	require_once(dirname(dirname(__FILE__)) . '/settings.php');
+	echo '<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Kehe</title>
+		<meta charset="UTF-8">
+	</head>
+	<main>
+	<div>
+		Kehe.
+	</div>
+	<a href="', $weblink, '">Home</a>
+	</main>
+</html>';
 }
 // Can't find it... just forget it.
 else
