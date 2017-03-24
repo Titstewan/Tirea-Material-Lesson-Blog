b7f6b876b2c7606191079cd68efbdee3203122e8
Tìtstewan <titstewan@learnnavi.org>
2017-03-24 10:09:50 +0100

    lul, need moar coffee

--- language/index.php
+++ language/index.php
@@ -1,7 +1,7 @@
 <?php
 // This file now doubles as a switcher to the language contained within
 setcookie('lang', $_REQUEST['lang'], time() + (86400 * 30), '/', $domain);
-//
+
 // Mooo
 if (isset($_SERVER['HTTP_REFERER']))
 {
