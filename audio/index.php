7c0a4d2b6e418395d061ed7a23558d0510bf079b
Tìtstewan <titstewan@learnnavi.org>
2017-03-26 12:55:50 +0200

    Redirect should work now

--- audio/index.php
+++ audio/index.php
@@ -1,24 +1,13 @@
 <?php
 // This file is here solely to protect this directory.
 define('TLB', 1);
-// Look for index.php....
+// This file is here solely to protect your Themes directory.
+// Look for settings.php....
 if (file_exists(dirname(dirname(__FILE__)) . '/settings.php'))
 {
 	// Found it!
-	require_once(dirname(dirname(__FILE__)) . '/settings.php');
-	echo '<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<title>Kehe</title>
-		<meta charset="UTF-8">
-	</head>
-	<main>
-	<div>
-		Kehe.
-	</div>
-	<a href="', $weblink, '">Home</a>
-	</main>
-</html>';
+	require(dirname(dirname(__FILE__)) . '/settings.php');
+	header('Location: ' . $weblink);
 }
 // Can't find it... just forget it.
 else
