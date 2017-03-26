9ae65615766e0e871142a07175bd2528888adf0c
Tìtstewan <titstewan@learnnavi.org>
2017-03-26 01:46:57 +0100

    undefined index fixed, lol.

--- language/index.php
+++ language/index.php
@@ -1,4 +1,7 @@
 <?php
+// this will give $doman a definition, lol.
+require_once(dirname(dirname(__FILE__) . '/index.php'));
+
 // This file now doubles as a switcher to the language contained within
 setcookie('lang', $_REQUEST['lang'], time() + (86400 * 30), '/', $domain);
 
