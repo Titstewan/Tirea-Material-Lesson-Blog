661899234ec8bfdbe7ae11bf4de221a32229f6b6
Tìtstewan <titstewan@learnnavi.org>
2017-03-26 04:04:19 +0200

    forgot define tlb...

--- language/index.php
+++ language/index.php
@@ -1,6 +1,9 @@
 <?php
+// This is kind of important.
+define('TLB', 1);
+
 // this will give $doman a definition, lol.
-require_once(dirname(dirname(__FILE__) . '/settings.php'));
+require_once(dirname(dirname(__FILE__)) . '/settings.php');
 
 // This file now doubles as a switcher to the language contained within
 setcookie('lang', $_REQUEST['lang'], time() + (86400 * 30), '/', $domain);
