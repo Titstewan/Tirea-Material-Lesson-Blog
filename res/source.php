1a6e3e60662b6e88a3f2f91fdf55035ee4a36015
Tirea Aean <tirea@learnnavi.org>
2018-10-07 09:28:48 -0400

    back to icon font for hamburger menu

--- res/source.php
+++ res/source.php
@@ -123,8 +123,8 @@ function html_header()
 			<nav class="indigo">
 				<div class="nav-wrapper">
 					<a href="', $weblink, '" class="brand-logo">Tirea Na&apos;vi</a>
-					<!-- <a href="#" data-activates="mobilenav" class="button-collapse"><i class="material-icons">menu</i></a> -->
-					<a class="button-collapse" href="#" data-activates="mobilenav"><span id="mobile-menu-icon">&#9776;</span></a>
+					<a href="#" data-activates="mobilenav" class="button-collapse"><i class="material-icons">menu</i></a>
+					<!-- <a class="button-collapse" href="#" data-activates="mobilenav"><span id="mobile-menu-icon">&#9776;</span></a> -->
 					<ul class="right hide-on-med-and-down" id="regnav">', $menu, '
 						<!-- Dropdown Trigger -->
 						<li><a class="dropdown-button" href="', $weblink, '" data-activates="dropdown1">', $txt['m_language'], '<i class="material-icons right">arrow_drop_down</i></a></li>
